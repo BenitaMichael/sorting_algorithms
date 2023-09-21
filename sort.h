6e461aa5ca5c66066208a76b393faa364605a28e
Benita Michael <benubiaka@gmail.com>
2023-09-21 11:10:38 -0700

    Too many commit

--- sort.h
+++ sort.h
@@ -18,9 +18,10 @@ typedef enum bool
 
 /**
  * struct listint_s - Doubly linked list node
- * @n: Integer in node
- * @prev: Pointer to the previous node
- * @next: Pointer to the next node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
@@ -42,5 +43,7 @@ void swap_function(int *a, int *b);
 /* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
