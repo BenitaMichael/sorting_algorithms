09302e1c237c30df4c0e69ca136ebc8c8567de76
Benita Michael <benubiaka@gmail.com>
2023-09-21 04:55:34 -0700

    First 3 done

---
+++ 2-selection_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * swap_function - function to swap two integers in an
+ * array using a temp storage
+ * @a: The first integer
+ * @b: The second integer
+ */
+void swap_function(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * selection_sort -  function that sorts an array of integers in
+ * ascending order using the Selection sort algorithm
+ * @array: An array of integers
+ * @size: The size of the array
+ * Description: prints the array after each swap
+ */
+void selection_sort(int *array, size_t size)
+{
+	int *min;
+	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
+
+		if ((array + i) != min)
+		{
+			swap_function(array + i, min);
+			print_array(array, size);
+		}
+	}
+}
+
---
+++ sort.h
@@ -0,0 +1,44 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * enum bool - Representation of Boolean values
+ * @false: Equals (0)
+ * @true: Equals (1)
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
+
+/**
+ * struct listint_s - Doubly linked list node
+ * @n: Integer in node
+ * @prev: Pointer to the previous node
+ * @next: Pointer to the next node
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+
+
+/* Given printing functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/* Swapping function */
+void swap_function(int *a, int *b)
+
+
+/* Sorting algoritms */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
