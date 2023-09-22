dbd886d381e1b8c240d21359d1ef62927bf1010c
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-09-22 11:04:48 -0700

    3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,4 @@
 #include "sort.h"
-
 void swap_function(int *a, int *b);
 int lomuto_partition(int *array, size_t size, int left, int right);
 void lomuto_sort(int *array, size_t size, int left, int right);
